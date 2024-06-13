db9298766c95c22388e3f70be18296e557ef9ec6
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-06-13 07:52:01 +0530

    minor fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -93,7 +93,7 @@ const loginUser = asyncHandler(async (req,res) => {
 
     const {email, username, password} = req.body
 
-    if (!username || !email) {
+    if (!(username || email)) {
         throw new ApiError(400,"usernamw or email is required")
     }
 

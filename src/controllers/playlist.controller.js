36258fbd959c7f8729b061ae6b146efe93f15bd3
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-06-23 23:27:57 +0530

    chore: Update playlist controller to handle CRUD operations for playlists

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -39,11 +39,36 @@ const createPlaylist = asyncHandler(async (req, res) => {
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+
+    const playlists = await Playlist.find({
+        owner: userId
+    })  //.populate("videos")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        playlists,
+        "User playlists retrieved successfully"
+    ))
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist ID")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        playlist,
+        "Playlist retrieved successfully"
+    ))
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
@@ -59,12 +84,52 @@ const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     // TODO: delete playlist
+
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist ID")
+    }
+
+    const playlist = await Playlist.findByIdAndDelete(playlistId)
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        playlist,
+        "Playlist deleted successfully"
+    ))
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
     //TODO: update playlist
+
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist ID")
+    }
+
+    if (!name || !description) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const playlist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            name,
+            description
+        },
+        {new: true}
+    )
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        playlist,
+        "Playlist updated successfully"
+    ))
+
 })
 
 export {

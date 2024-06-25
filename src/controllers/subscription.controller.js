fb56e72a0afcb558ff0dd7ebc060656706883345
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-06-25 11:23:27 +0530

    chore: Implement toggleSubscription functionality in subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,6 +9,45 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+
+    const subscriberId = req.user._id
+
+    const channel = await User.findById(channelId)
+
+    if (!channel) {
+        throw new ApiError(404, "Channel not found")
+    }
+
+    const subscription = await Subscription.findOne({
+        subscriber: subscriberId,
+        channel: channelId
+    })
+
+    if (subscription) {
+        
+        await Subscription.findByIdAndDelete(subscription._id)
+
+        return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            {},
+            "Unsubscribed successfully"
+        ))
+    }
+
+    await Subscription.create({
+        subscriber: subscriberId,
+        channel: channelId
+    })
+
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        {},
+        "Subscribed successfully"
+    ))
 })
 
 // controller to return subscriber list of a channel

9b430c874d872d347b553c6266b3603795947212
Kshitij Gehlot <kdgehlot2003@gmail.com>
2024-06-27 22:44:49 +0530

    added workinf frontend with shadcn integration

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -10,11 +10,102 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
+    
+    const pipeline = []
+
+    console.log(userId);
+
+    // Match stage for filtering by userId
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid userId!");
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+        throw new ApiError(404, "User Not available witht this userId!");
+    }
+
+    if (userId) {
+        pipeline.push({
+            $match: {
+                owner: mongoose.Types.ObjectId (userId)
+            }
+        })   
+    }
+
+    // Match stage for based on text query
+
+    if (query){
+        pipeline.push({
+            $match: {
+                $or: [
+                    { title: { $regex: query, $options: 'i' } },
+                    { description: { $regex: query, $options: 'i' } }
+                ]
+            }
+        })
+    }
+
+    // sort stage
+
+    if (sortBy && sortType) {
+        const sortTypeValue = sortType === 'desc' ? -1 : 1;
+        pipeline.push({
+            $sort: {[sortBy]: sortTypeValue}
+        })
+    }
+
+    // populate the owner
+
+    pipeline.push({
+        $lookup: {
+            from: "users",
+            localField: "owner",
+            foreignField: "_id",
+            as: "owner",
+            pipeline: [
+                {
+                    $project: {
+                        username: 1,
+                        fullName: 1,
+                        avatar: 1
+                    }
+                }
+            ]
+        }
+    })
+
+    // add the calculated owner field
+    pipeline.push({
+        $addFields: {
+            owner: {
+                $first: "$owner"
+            }
+        }
+    })
+
+    const aggregate = Video.aggregate(pipeline)
+
+    Video.aggregatePaginate(aggregate, { page, limit })
+        .then(function (result) {
+            return res.status(200).json(new ApiResponse(
+                200,
+                { result },
+                "Fetched videos successfully"
+            ))
+        })
+        .catch(function (error) {
+            throw error
+        })
+
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
